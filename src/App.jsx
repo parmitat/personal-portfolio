c930ba9478c91c3ccb177f611ceff6110c71f852
parmitat <tparmita18506@gmail.com>
2025-07-24 21:14:46 +0530

    Fix: ensure correct output directory and SPA routing

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { Home } from "./components/sections/Home";
 import { About } from "./components/sections/About";
 import { Projects } from "./components/sections/Projects";
 import { Contact } from "./components/sections/Contact";
+import emailjs from '@emailjs/browser';
 import "./index.css";
 
 function App() {
